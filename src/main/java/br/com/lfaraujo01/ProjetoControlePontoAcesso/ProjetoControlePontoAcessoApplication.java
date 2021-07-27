5fb27deef4b166deca0b921e871769f73d4e2c3d
lfaraujo01 <lucas_araujo01@hotmail.com>
2021-07-27 10:55:05 -0300

    Project Creation

---
+++ src/main/java/br/com/lfaraujo01/ProjetoControlePontoAcesso/ProjetoControlePontoAcessoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lfaraujo01.ProjetoControlePontoAcesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoControlePontoAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoControlePontoAcessoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lfaraujo01/ProjetoControlePontoAcesso/ProjetoControlePontoAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lfaraujo01.ProjetoControlePontoAcesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoControlePontoAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
